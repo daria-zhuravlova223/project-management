66941516aa6de1afc847f8c53fd739ea9164a6bc
daria <daria.zhuravlova23@gmail.com>
2024-10-30 19:18:07 +0200

    update image urls

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -64,7 +64,7 @@ const Sidebar = () => {
 
         {/* TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="logo" width={40} height={40} />
+          <Image src="https://projmanag-s3-images.s3.us-east-1.amazonaws.com/logo.png" alt="logo" width={40} height={40} />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               EDROH TEAM
