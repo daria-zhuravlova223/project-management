683c06554e0969d503b11b740f65967138f2eaeb
daria <daria.zhuravlova23@gmail.com>
2024-10-31 14:19:48 +0200

    add create user route

--- server/dist/src/controllers/userController.js
+++ server/dist/src/controllers/userController.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUsers = void 0;
+exports.postUser = exports.getUsers = void 0;
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -26,3 +26,20 @@ const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 exports.getUsers = getUsers;
+const postUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username, cognitoId, profilePictureUrl = 'i1.jpeg', teamId = 1, } = req.body;
+        const newUser = yield prisma.user.create({
+            data: { username, cognitoId, profilePictureUrl, teamId },
+        });
+        res.json({ message: 'User created Successfully', newUser });
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }
+    catch (error) {
+        console.log(error);
+        res
+            .status(500)
+            .json({ message: `Error retrieving users: ${error.message}` });
+    }
+});
+exports.postUser = postUser;
--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -16,3 +16,25 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
       .json({ message: `Error retrieving users: ${error.message}` });
   }
 };
+
+export const postUser = async (req: Request, res: Response) => {
+  try {
+    const {
+      username,
+      cognitoId,
+      profilePictureUrl = 'i1.jpeg',
+      teamId = 1,
+    } = req.body;
+    const newUser = await prisma.user.create({
+      data: { username, cognitoId, profilePictureUrl, teamId },
+    });
+
+    res.json({ message: 'User created Successfully', newUser });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: `Error retrieving users: ${error.message}` });
+  }
+};
